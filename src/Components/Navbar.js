7ecb041a8dbd7bd1ad0d67c0932cfc5ae17b2634
Aniket <aniketupatel1@gmail.com>
2019-12-14 16:53:38 -0500

    created foundation components for pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,18 +14,18 @@ function Navbar() {
         <Layout>
             <Header title="Title" scroll>
                 <Navigation>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
                 </Navigation>
             </Header>
             <Drawer title="Title">
                 <Navigation>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
                 </Navigation>
             </Drawer>
             <Content>
